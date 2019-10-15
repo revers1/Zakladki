d9b67b30e016eac1750e3ca408c4760fdb802b20
romaprozhoga <romaprozhoga@A25PC5>
2019-10-15 20:33:19 +0300

    CreatedGearHostBaseDB

---
+++ ApiZakladokNet/Entity/Product.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiZakladokNet.Entity
+{
+    public class Product
+    {
+    }
+}
