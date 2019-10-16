ead2d78892e351ae35408a37c0655a24a54b54e1
Veremeychuk <Veremeychukden@gmail.com>
2019-10-16 18:58:04 +0300

    ProdController

---
+++ ApiZakladokNet/Controllers/ProductController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ApiZakladokNet.Entity;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiZakladokNet.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly EFContext context;
+        private readonly IHostingEnvironment appEnvironment;
+        public ProductController(EFContext ccontext, IHostingEnvironment appenv)
+        {
+            context = ccontext;
+            appEnvironment = appenv;
+        }
+
+        
+    }
+}
