7f6f181dfab7367b9937676e9b8ce3f11073e8fc
Veremeychuk <Veremeychukden@gmail.com>
2019-10-23 20:42:00 +0300

    ashdjkagdkahkdhakdw

--- ApiZakladokNet/Migrations/20191023171618_123.Designer.cs
+++ ApiZakladokNet/Migrations/20191023171618_123.Designer.cs
@@ -3,14 +3,16 @@ using ApiZakladokNet.Entity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace ApiZakladokNet.Migrations
 {
     [DbContext(typeof(EFContext))]
-    partial class EFContextModelSnapshot : ModelSnapshot
+    [Migration("20191023171618_123")]
+    partial class _123
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
