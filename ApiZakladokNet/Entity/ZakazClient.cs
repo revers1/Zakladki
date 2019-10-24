e4e4750240370b46165532b2aef3dbf3e08bb943
roma.prozhoga <roma.prozhoga@gmail.com>
2019-10-24 18:29:30 +0300

    addclientzakaz

---
+++ ApiZakladokNet/Entity/ZakazClient.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiZakladokNet.Entity
+{
+    [Table("ZakazClient")]
+    public class ZakazClient
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public float Quantity { get; set; }
+
+        public string Description { get; set; }
+
+
+
+    }
+}
---
+++ ZakladkiAdoNet/ZakazClient.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZakladkiAdoNet
+{
+    public class ZakazClient
+    {
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public float Quantity { get; set; }
+  
+    }
+}
